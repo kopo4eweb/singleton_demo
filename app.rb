462afb71464ddf0733caf17ed8ca826fbc52b55b
kopo4eweb <kopo4e.web@yandex.ru>
2020-04-12 16:28:02 +0300

    update logger

--- app.rb
+++ app.rb
@@ -3,16 +3,30 @@ class Logger
         @f = File.open('log.txt', 'a')
     end
 
+    @@x = nil
+
+    def self.instance
+        if @@x == nil
+            @@x = Logger.new
+        end
+
+        return @@x
+    end
+
+    # class method
     def self.say_something
         puts "Haha"
     end
 
+    # instance method
     def log_something wat
         @f.puts wat
     end
 end
 
 Logger.say_something
+Logger.instance.log_something 'Blabla'
+Logger.instance.log_something 'Blabla22'
 
 logger = Logger.new
 logger.log_something 'Hey!'
